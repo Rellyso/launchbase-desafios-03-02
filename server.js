9a356561dff3c1147bdae5e1549db6997e5afedb
Rellyson <rellysondouglas2015@gmail.com>
2020-06-24 08:49:00 -0300

    Desafios 03 pt.1 / Primeiro servidor

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+
+// 5 - configurando para utilizar estilos na pasta estática public/
+server.use(express.static('public'))
+
+// 3 - setar mecanismo de visualização como o html
+server.set("view engine", "njk")
+
+// 2 - configurar nunjucks para o servidor procurar na pasta views
+nunjucks.configure('views', {
+    express: server
+})
+
+// 4 - renderizar página principal (/) com o get (req, res) retornando res.render('index')
+server.get('/', function (req, res) {
+    return res.render('about')
+} )
+
+server.get('/courses', (req, res) => {
+    return res.render('courses')
+})
+
+server.use(function (req,res) {
+    res.status(404).render('not-found')
+})
+
+// 1 - configurando porta do servidor 
+server.listen(5000, function () {
+    console.log('server is running')
+})
