2c31ca7f4f9a68600a26e52bfe33b1977cc9b94f
Rellyson <rellysondouglas2015@gmail.com>
2020-06-25 18:51:20 -0300

    preparing for a course page

---
+++ data.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        id: 1,
+        name: "starter",
+        img: "assets/icons/rckt_starter.svg",
+        info: "<strong>Torne-se um programador desejado</strong> no mercado com esses cursos gratuitos.",
+        modules: "5 módulos",
+        price: "Free"
+    },
+    {
+        id: 2,
+        name: "launchbase",
+        img: "assets/icons/rckt_launch.svg",
+        info: "<strong>Domine a programação do zero</strong> e tenha acesso às melhores oportunidades do mercado.",
+        modules: "10 módulos",
+        price: "R$ 1.000,00"
+    },
+    {
+        id: 1,
+        name: "gostack",
+        img: "assets/icons/rckt_goStack.svg",
+        info: "<strong>Treinamento imersivo</strong> nas tecnologias mais modernas de desenvolvimento web e mobile.",
+        modules: "8 módulos",
+        price: "R$ 2.9000,00"
+    }
+]
--- server.js
+++ server.js
@@ -13,21 +13,52 @@ server.set("view engine", "njk")
 
 // 2 - configurar nunjucks para o servidor procurar na pasta views
 nunjucks.configure('views', {
-    express: server
+    express: server,
+    // permitir ler html
+    autoescape: false,
+    noCache: true
 })
 
 // 4 - renderizar página principal (/) com o get (req, res) retornando res.render('index')
 server.get('/', function (req, res) {
-    return res.render('about')
+    const about = {
+        name: "Rocketseat",
+        img: "https://rocketseat.gallerycdn.vsassets.io/extensions/rocketseat/rocketseatreactnative/3.0.1/1588456740326/Microsoft.VisualStudio.Services.Icons.Default",
+        description: 'Uma plataforma de educação em tecnologia, somos uma comunidade incrível de programadores em busca do próximo nível. Acesse <a href="http://rocketseat.com.br" target="_blank" rel="noopener noreferrer">Rocketseat</a>.',
+        technologies: [
+            {name: "Javascript"},
+            {name: "HTML"},
+            {name: "CSS"},
+            {name: "Nunjucks"},
+            {name: "SQL"}
+        ]
+    }
+    return res.render('about', {about})
 } )
 
 server.get('/courses', (req, res) => {
-    return res.render('courses')
+    return res.render('courses', {courses})
+})
+
+server.get('/courses/:id', function (req, res) {
+    
+    const id = req.params.id
+
+    const course = courses.find(function (course) {
+        return course.name == id
+    })
+
+    if ( !course ) {
+        return res.render('not-found')
+    }
+
+    return res.render('course', {course})
 })
 
 server.use(function (req,res) {
     res.status(404).render('not-found')
 })
+
 
 // 1 - configurando porta do servidor 
 server.listen(5000, function () {
