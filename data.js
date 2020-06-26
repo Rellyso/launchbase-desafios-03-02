384c4a0fdb3f9fccec0208341e09717456b987d6
Rellyson <rellysondouglas2015@gmail.com>
2020-06-26 10:16:45 -0300

    Added style on course's page

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ module.exports = [
     {
         id: 1,
         name: "starter",
-        img: "assets/icons/rckt_starter.svg",
+        img: "/assets/icons/rckt_starter.svg",
         info: "<strong>Torne-se um programador desejado</strong> no mercado com esses cursos gratuitos.",
         modules: "5 módulos",
         price: "Free"
@@ -10,7 +10,7 @@ module.exports = [
     {
         id: 2,
         name: "launchbase",
-        img: "assets/icons/rckt_launch.svg",
+        img: "/assets/icons/rckt_launch.svg",
         info: "<strong>Domine a programação do zero</strong> e tenha acesso às melhores oportunidades do mercado.",
         modules: "10 módulos",
         price: "R$ 1.000,00"
@@ -18,7 +18,7 @@ module.exports = [
     {
         id: 1,
         name: "gostack",
-        img: "assets/icons/rckt_goStack.svg",
+        img: "/assets/icons/rckt_goStack.svg",
         info: "<strong>Treinamento imersivo</strong> nas tecnologias mais modernas de desenvolvimento web e mobile.",
         modules: "8 módulos",
         price: "R$ 2.9000,00"
--- public/cursos.js
+++ public/cursos.js
@@ -1,40 +1,8 @@
-const modalOverlay = document.querySelector('.modal-overlay')
-const modal = document.querySelector('.modal')
 const cards = document.querySelectorAll('.card')
 
 for (let card of cards) {
     card.addEventListener('click', function () {
         const siteID = card.getAttribute('id')
-        modalOverlay.classList.add('active')
-        modalOverlay.querySelector('iframe').src = `https://rocketseat.com.br/${siteID}`
-        modal.querySelector('.max-modal').classList.add('active')
+        window.location.href = `courses/${siteID}`
     })
-}
-
-modalOverlay.querySelector('.close-modal').addEventListener('click', function () {
-    modalOverlay.classList.remove('active')
-    modalOverlay.querySelector('iframe').src = ''
-    minimizeModal()
-    modal.querySelector('.max-modal').classList.remove('active')
-})
-
-modal.querySelector('.max-modal').addEventListener('click', function () {
-    if (modal.classList.contains('maximize')) {
-        minimizeModal()
-    } else {
-        maximizeModal()
-    }
-})
-
-
-function maximizeModal() {
-    modal.classList.add('maximize')
-    modal.querySelector('.max-modal i#minimize').classList.add('active')
-    modal.querySelector('.max-modal i#maximize').classList.remove('active')
-}
-
-function minimizeModal() {
-    modal.classList.remove('maximize')
-    modal.querySelector('.max-modal i#maximize').classList.add('active')
-    modal.querySelector('.max-modal i#minimize').classList.remove('active')
 }
